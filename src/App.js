04c6dfe29016ebfcea08f78cd06817686a28e13f
vandanachadha <vishuprajapati15@gmail.com>
2023-10-10 14:52:31 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,103 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {loadStripe} from '@stripe/stripe-js';
+import {
+  PaymentElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js';
+
+
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (elements == null) {
+      return;
+    }
+
+    // elements.create("expressCheckout")
+
+    // Trigger form validation and wallet collection
+    const {error: submitError} = await elements.submit();
+    if (submitError) {
+      // Show error to your customer
+      setErrorMessage(submitError.message);
+      return;
+    }
+
+    // Create the PaymentIntent and obtain clientSecret from your server endpoint
+    const res = await fetch('/create-intent', {
+      method: 'POST',
+    });
+
+    const {client_secret: clientSecret} = {client_secret: "sk_test_aediX53shAzQkRvioEKu6CXV"}
+
+    console.log(clientSecret)
+
+    const {error} = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      clientSecret,
+      confirmParams: {
+        return_url: 'http://localhost:3000/',
+      },
+    });
+
+    if (error) {
+      // This point will only be reached if there is an immediate error when
+      // confirming the payment. Show error to your customer (for example, payment
+      // details incomplete)
+      setErrorMessage(error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <PaymentElement />
+      <button type="submit" disabled={!stripe || !elements}>
+        Pay
+      </button>
+      {/* Show error message to your customers */}
+      {errorMessage && <div>{errorMessage}</div>}
+    </form>
+  );
+};
+
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_W3AWlvrzo3jtQnDsKtyCGCUS');
+
 
 function App() {
+
+  const options = {
+    // passing the client secret obtained from the server
+    // clientSecret: `sk_test_aediX53shAzQkRvioEKu6CXV`
+    mode: "payment",
+    amount: 1099,
+    currency: "usd"
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Elements stripe={stripePromise} options={options}>
+          <CheckoutForm />
+        </Elements>
       </header>
     </div>
   );
